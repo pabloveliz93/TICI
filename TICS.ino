bacb90f00b5aeb9360766244803042a42c4c2309
MichiruNakam <michiru.nakamura@mail.udp.cl>
2017-05-30 02:32:26 -0300

    Update

--- TICS.ino
+++ TICS.ino
@@ -1,74 +1,62 @@
-#include <OneWire.h> //Se importan las librerías
-#include <DallasTemperature.h>
- 
 #define TEMPERATURE_PIN 3//Se declara el pin donde se conectará la DATA
-#define VOL_CODE 9999 // El identificador de datos de volumen
+#define PING_PIN 2 //Pin para sensor volumen
+#define FLUX_PIN 0 //Pin para sensor de flujo
+#define VOL_SETUP_CODE 9999 // El identificador de datos de SETUP de volumen
+#define VOL_CODE 9997 // El identificador de datos de volumen
 #define TEMP_CODE 9998 //El identificador de datos de temperatura
+#define FLUX_CODE 9996 //El identificador de datos de flujo
 #define END_CODE -1.0 //Indica que ha finalizado el envío de un burst de datos
-const int pingPin = 2;
 
-//Necesario para sensor de temperatura
-OneWire ourWire(TEMPERATURE_PIN); //Se establece el pin declarado como bus para la comunicación OneWire
+#include <NewPing.h> //Para sensor de volumen
+NewPing sonar(PING_PIN, PING_PIN );
+
+#include <OneWire.h> //Para sensor de temperatura
+#include <DallasTemperature.h>
+OneWire ourWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&ourWire);
 
-
-void setup() {
-  // initialize serial communication:
-  Serial.begin(9600);
-  sensors.begin();
+void setup(){
+  Serial.begin(9600);          //  setup serial
+  sensors.begin();             //  setup sensor de temperatura
 }
-
-void loop() {
-  
-  long duration, inches;
-  float cm;
-  short int identifier_code; //Codigo identificador
-  int k = Serial.available();
-  if (k > 0) {
-    if (k == 4){
-      identifier_code = get_incomming_code_4();
-    }
-    //Serial.print(identifier_code);
-    if (identifier_code == VOL_CODE){
-      //Serial.print(VOL_CODE);
-      //Serial.println();
-      for (int i = 0; i <10; i++){
-        pinMode(pingPin, OUTPUT);
-        digitalWrite(pingPin, LOW);
-        delayMicroseconds(2);
-        digitalWrite(pingPin, HIGH);
-        delayMicroseconds(5);
-        digitalWrite(pingPin, LOW);
-        pinMode(pingPin, INPUT);
-        duration = pulseIn(pingPin, HIGH);
-      
-        //inches = microsecondsToInches(duration);
-        cm = microsecondsToCentimeters(duration);
-      
-        Serial.print(cm);
-        Serial.println();
-      
-        delay(100);
+void loop(){
+    short int code;
+    int k = Serial.available();
+    if (k > 0) {
+      if (k == 4){
+        code = get_incomming_code_4();
       }
-    //Al final enviamos -1 para indicar que se ha terminado el envío de datos
-    Serial.print(END_CODE);
-    Serial.println();
+    switch (code){
+      case FLUX_CODE:
+        //for (int i = 0; i < 100; i ++){
+        int flux = analogRead(FLUX_PIN);
+        Serial.println(flux);
+        delay(100);
+        //}
+      break;
+      case VOL_CODE:
+        //for (int i = 0; i < 100; i ++){
+        float uS = sonar.ping();
+        Serial.println((float)(uS / US_ROUNDTRIP_CM));
+        delay(100);
+        //}
+      break;
+      case VOL_SETUP_CODE:
+        for (int i = 0; i <10; i++){
+          float uS = sonar.ping();
+          Serial.println((float)(uS / US_ROUNDTRIP_CM));
+          delay(100);
+        }
+        //Al final enviamos -1 para indicar que se ha terminado el envío de datos
+        Serial.println(END_CODE);
+      break;
+      case TEMP_CODE:
+        sensors.requestTemperatures(); //Prepara el sensor para la lectura
+        Serial.println(sensors.getTempCByIndex(0)); //Se lee e imprime la temperatura en grados Celsius
+        delay(1000);
+      break;
     }
-  if (identifier_code == TEMP_CODE){
-    sensors.requestTemperatures(); //Prepara el sensor para la lectura
-    //Serial.print(TEMP_CODE);
-    //Serial.println();
-    Serial.print(sensors.getTempCByIndex(0)); //Se lee e imprime la temperatura en grados Celsius
-    Serial.println();
-    //Serial.print(sensors.getTempFByIndex(0)); //Se lee e imprime la temperatura en grados Fahrenheit
-    //Serial.println(" grados Fahrenheit"); 
-    delay(1000);
   }
-  }
-}
-
-float microsecondsToCentimeters(float microseconds) {
-  return microseconds / 29.0 / 2.0;
 }
 
 int get_incomming_code_4(){
@@ -76,4 +64,3 @@ int get_incomming_code_4(){
   for (int i = 0; i < 4; ++i) incommingcode[i] = Serial.read();
   return atoi(incommingcode);
 }
-
